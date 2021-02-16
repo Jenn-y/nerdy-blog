f3d3f326825a5e2e4c5c5a182c9c431500a3afe8
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-02-16 21:39:44 +0100

    refactored with functions and hooks

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,18 @@ import React from 'react';
 import '../App.css';
 import { Link } from "react-router-dom";
 
-class App extends React.Component{
-  render(){
-    return (
-      <div className="welcome-container">
-        <div>
-          <h1>HOME</h1>
-          <Link to="/post1"><button>Read Post #1</button></Link>
-          <Link to="/post2"><button>Read Post #2</button></Link>
-        </div>
-        <div className="footer">
-          <p>2021   |   Jenny</p>
-        </div>
+const App = () => {
+  return (
+    <div className="welcome-container">
+      <div>
+        <h1>HOME</h1>
+        <Link to="/post1"><button>Read Post #1</button></Link>
+        <Link to="/post2"><button>Read Post #2</button></Link>
       </div>
-    )
-  }
+      <div className="footer">
+        <p>2021   |   Jenny</p>
+      </div>
+    </div>
+  )
 }
 export default App;
--- src/components/Post2.js
+++ src/components/Post2.js
@@ -1,38 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Post from '../Post2.md';
 import ReactMarkdown from 'react-markdown';
 import '../Post.css';
 import gfm from 'remark-gfm';
 import { Link } from "react-router-dom";
 
-class Post2 extends React.Component{
-  constructor(){
-    super();
-    this.state = { markdown: '' };
-  }
-  componentDidMount(){
-    fetch(Post).then(res => res.text()).then(text => this.setState({markdown: text}));
-  }
-  render(){
-    const {markdown} = this.state;
-    return (
-      <div>
-        <div className="header">
-          <div className="navigation">
-            <Link to="/"><button>Home</button></Link>
-            <Link to="/post1"><button>Read Post #1</button></Link>
-            <Link to="/post2"><button className="active">Read Post #2</button></Link>
-          </div>
-          <h2>Blog Post #2</h2>
+const Post2 = () => {
+  const [markdown, setMarkdown] = useState();
+
+  useEffect(() => {
+    fetch(Post).then(res => res.text()).then(text => setMarkdown(text));
+  }, []);
+
+  return (
+    <div>
+      <div className="header">
+        <div className="navigation">
+          <Link to="/"><button>Home</button></Link>
+          <Link to="/post1"><button>Read Post #1</button></Link>
+          <Link to="/post2"><button className="active">Read Post #2</button></Link>
         </div>
-        <div className="container">
-          <ReactMarkdown plugins={[gfm]} source={markdown} />
-        </div>
-        <div className="footer">
-          <p>2021   |   Jenny</p>
-        </div>
+        <h2>Blog Post #2</h2>
       </div>
-    )
-  }
+      <div className="container">
+        <ReactMarkdown plugins={[gfm]} source={markdown} />
+      </div>
+      <div className="footer">
+        <p>2021   |   Jenny</p>
+      </div>
+    </div>
+  )
 }
 export default Post2;
